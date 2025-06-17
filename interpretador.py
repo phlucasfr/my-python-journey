96a4ba30a26462e9d8b4647fd067080658a9593d
Phelipe <phlucasfr@gmail.com>
2025-06-17 11:15:43 -0300

    3. Uma introdução informal ao Python - 3.1.2. Texto

---
+++ interpretador.py
@@ -0,0 +1,31 @@
+# este é o primeiro comentário
+spam = 1 
+texto = "# Este não é um comentário por estar entre aspas"
+
+# Hello flat earth example kkk
+o_mundo_é_plano = True
+if o_mundo_é_plano:
+    print("Cuidado para não cair da borda!")
+
+print(17 / 3) # divisão clássica retorna um ponto flutuante
+print(17 // 3) # divisão pelo piso descarta a parte fracionária
+print(17 % 3) # retorna o resto da divisão
+
+print(5 ** 2) # python suporta calculo de potência
+
+# operador '=' para atribuição em python
+largura = 20
+
+# exemplo de variavel não definida
+# n
+
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# NameError: name 'n' is not defined
+
+# operadores com operandos de diferentes tipos convertem o inteiro para ponto flutuante:
+print(4 * 3.75 - 1)
+# 14.0
+
+print('spam eggs') # aspas simples
+print("Coelho de Paris, eu te projeto :)! Viva!") # aspas duplas
