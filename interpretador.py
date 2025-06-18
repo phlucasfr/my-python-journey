4338b9b4485b19eccf5d782a1c0538a874f5f547
Phelipe <phlucasfr@gmail.com>
2025-06-18 10:41:54 -0300

    3. Uma introdução informal ao Python - 3.1.2. Texto - end

--- interpretador.py
+++ interpretador.py
@@ -29,3 +29,36 @@ print(4 * 3.75 - 1)
 
 print('spam eggs') # aspas simples
 print("Coelho de Paris, eu te projeto :)! Viva!") # aspas duplas
+
+print('d\'água') # usar \' para escapar a aspa simples...
+print("d'água") # ou usar aspas duplas
+print('"Sim", eles disseram')
+print("\"Sim\", eles disseram")
+
+print(r'C:\algum\nome')  # observar o r antes das aspas
+
+#strings literais 
+print("""\
+    Uso: coisinha [OPÇÕES]
+        -h                        Exibe esta mensagem de uso
+        -H hostname               Hostname para se conectar
+      """)
+
+print(3 * 'un' + 'ium') # Strings podem ser concatenadas (coladas) com o operador +, e repetidas com *
+
+print('Py' 'thon') # Contatenação automagica
+
+prefixo = 'Py'
+
+print(prefixo + 'thon')
+
+palavra = 'Python'
+print(palavra[0]) #Indexação
+
+print(palavra[:2]) # Fatiamento
+
+#strings em python são imutáveis
+
+s = 'supercalifragilisticexpialidoce'
+print(len(s)) # comprimento de uma string
+
