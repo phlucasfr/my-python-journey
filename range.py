d965dbdbdd27cb9effaf0f441eb01eafe426a0c9
Phelipe <phlucasfr@gmail.com>
2025-06-23 09:29:54 -0300

    4. Mais ferramentas de controle de fluxo - 4.1. A função range - end

---
+++ range.py
@@ -0,0 +1,11 @@
+for i in range(5):
+    print(i)
+
+a = list(range(5, 10))
+print(a)
+
+a = ['Maria', 'tinha', 'um', 'carneirinho']
+for i in range(len(a)):
+    print(i, a[i])
+
+print(sum(range(4)))
