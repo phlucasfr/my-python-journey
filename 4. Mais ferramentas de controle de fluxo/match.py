0846dacf5629156a6fa3612bc4b0ae2afc72266f
Phelipe <phlucasfr@gmail.com>
2025-06-28 11:12:06 -0300

    4.7. Instruções match - padrões com literais e variáveis

--- 4. Mais ferramentas de controle de fluxo/match.py
+++ 4. Mais ferramentas de controle de fluxo/match.py
@@ -2,9 +2,44 @@ def http_error(status):
     match status:
         case 400:
             return "Bad request"
-        case 404:
-            return "Not found"
+        case 401 | 403 | 404: # multiplos cases
+            return "Não permitido"
         case 418:
             return "I'm a teapot"
         case _: # default
             return "Something's wrong with the internet"
+        
+    
+ponto = (2, 3)
+match ponto:
+    case(0, 0):
+        print("Origem")
+    case(0,y):
+        print(f"Y={y}")
+    case(x,0):
+        print(f"X={x}")
+    case(x,y):
+        print(f"X={x} ,Y={y}")
+    case _:
+        raise ValueError("Não é um ponto")
+    
+class Ponto:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def onde_esta(ponto):
+    match ponto:
+        case Ponto(x=0, y=0):
+            print("Origem")
+        case Ponto(x=0, y=y):
+            print(f"Y={y}")
+        case Ponto(x=x, y=0):
+            print(f"X={x}")
+        case Ponto():
+            print("Em outro lugar")
+        case _:
+            print("Não é um ponto")
+
+ponto = Ponto(0,2)
+onde_esta(ponto)        
