20e8d77de220f3d6638d21aa92bb67b70af2ff96
Phelipe <phlucasfr@gmail.com>
2025-07-02 09:52:27 -0300

    4.9.2. Argumentos nomeados

--- 4. Mais ferramentas de controle de fluxo/funcoes.py
+++ 4. Mais ferramentas de controle de fluxo/funcoes.py
@@ -36,3 +36,18 @@ def pergunta_ok(mensagem, tentativas=4, lembrete='Por favor, tente novamente!'):
 
 pergunta_ok('Deseja continuar?')
 
+def loja_de_queijos(tipo, *argumentos, **argumentos_nomeados):
+    print("-- Você tem algum", tipo, "?")
+    print("-- Lamento, acabou o", tipo)
+    for arg in argumentos:
+        print(arg)
+    print("-" * 40)
+    for kw in argumentos_nomeados:
+        print(kw, ":", argumentos_nomeados[kw])
+
+loja_de_queijos("Limburger", "Está muito mole, senhor",
+           "Está realmente muito, MUITO mole, senhor.",
+           vendedor="Michael Palin",
+           cliente="John Cleese",
+           sketch="Sketch da Loja de Queijos")
+
