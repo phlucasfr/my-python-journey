a069df022361e4c54dcabc8d1f00cc588a22eb0e
Phelipe <phlucasfr@gmail.com>
2025-07-12 09:09:03 -0300

    5.1.3. Compreensões de lista - Usando if

--- 5. Estruturas de dados/compreensoes_de_listas.py
+++ 5. Estruturas de dados/compreensoes_de_listas.py
@@ -8,3 +8,15 @@ print(quadrados)
 
 quadrados = [x**2 for x in range(10)] # mais conciso e legível
 print(quadrados)
+
+# que coisa feia
+print([(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y])
+
+#equivalente
+combos = []
+for x in [1, 2, 3]:
+    for y in [3, 1, 4]:
+        if x != y:
+            combos.append((x, y))
+
+print(combos)
